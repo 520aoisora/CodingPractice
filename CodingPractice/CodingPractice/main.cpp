53ac0752982e1cc0641c4676727c6d4fdd76e18c
520aoisora <520aoisora@gmail.com>
2013-09-21 23:27:33 +0100

    Repo practice

---
+++ CodingPractice/CodingPractice/main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+#include "Question1.h"
+#include "Question2.h"
+#include "Question3.h"
+#include "Question4.h"
+#include "Question5.h"
+#include "Question6.h"
+#include "Question7.h"
+
+int main()
+{
+	Q7::run();
+
+	return 0;
+}
